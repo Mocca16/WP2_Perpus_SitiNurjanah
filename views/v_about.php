f967ff85384ada0fd486fbc06851c3369d9866b8
Mocca16 <19221075@bsi.ac.id>
2023-10-26 21:36:11 +0700

    Selesai

---
+++ views/v_about.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>About</title>
+    </head>
+    <body>
+        <section>
+            <h1><?php echo $judul ?></h1>
+            <h4>Nama</h4>
+            <ul type="disc">
+                <li>Nama Depan      : Siti</li>
+                <li>Nama Belakang   : Nurjanah</li>
+            </ul>
+            <br>
+            <h4>Alamat</h4>
+            <ul type="none">
+                <li>Jln. Kramat Sentiong No. 51</li>
+            </ul>
+
+            <h4>Tempat Lahir</h4>
+            <ul type="none">
+                <li>Bandung</li>
+            </ul>
+
+            <h4>Olahraga Favorit</h4>
+            <ul type="square">
+                <li>Bulutangkis</li>
+                <li>Volly</li>
+            </ul>
+        </selection>
+    </body>
+</html>
---
+++ views/v_footer.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Rental Buku</title>
+    </head>
+    <body>
+        <div>
+            <footer>
+            <a href=”http://www.RentalBuku.com”>Siti Nurjanah</a>
+            </footer>
+        </div>
+    </body>
+</html>
